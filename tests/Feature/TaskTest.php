df5a613120698bf15a84d1bb9fc6959968a74441
shashika nuwan <kumararanaweera1999@gmail.com>
2021-12-25 13:01:45 +0530

    store and destroy task

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -11,15 +11,35 @@ class TaskTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_fetch_all_items_of_a_todo_list()
+    public function test_fetch_all_tasks_of_a_todo_list()
     {
-        $task = Task::factory()->create();
-        
+        $task = $this->createTask();
+
         $response = $this->getJson(route('task.index'))
             ->assertOk()
             ->json();
 
         $this->assertEquals(1, count($response));
         $this->assertEquals($task->title, $response[0]['title']);
+    }
+
+    public function test_store_a_tasks_for_a_todo_list()
+    {
+        $task = Task::factory()->make();
+
+        $this->postJson(route('task.store', ['title' => $task->title]))
+            ->assertCreated();
+
+        $this->assertDatabaseHas('tasks', ['title' => $task->title]);
+    }
+
+    public function test_delete_a_tasks_for_a_todo_list()
+    {
+        $task = $this->createTask();
+
+        $this->deleteJson(route('task.destroy',  $task->id))
+            ->assertNoContent();
+
+        $this->assertDatabaseMissing('tasks', ['title' => $task->title]);
     }
 }
