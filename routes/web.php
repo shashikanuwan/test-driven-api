602a3d3ee861f1e4ea39b6df68e665312dcb777a
shashika nuwan <kumararanaweera1999@gmail.com>
2022-02-08 20:16:39 +0530

    upload txt file in google drive

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Google\Client;
+use Google\Service\Drive;
+use Google\Service\Drive\DriveFile;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,8 +11,8 @@ Route::get('/', function () {
 
 Route::get('/drive', function () {
     $client = new Client();
-    $client->setClientId('402045549560-ap4pj1o1tocjqpjeb062otqmf2mns91v.apps.googleusercontent.com');
-    $client->setClientSecret('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');
+    $client->setClientId('402045549560-gck8s60du9u1kj3e1m0leq1bc1v2jqaf.apps.googleusercontent.com');
+    $client->setClientSecret('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');
     $client->setRedirectUri('http://localhost:8000/google-drive/callback');
     $client->setScopes([
         'https://www.googleapis.com/auth/drive',
@@ -22,7 +24,37 @@ Route::get('/drive', function () {
 
 Route::get('/google-drive/callback', function () {
     $client = new Client();
+    $client->setClientId('402045549560-gck8s60du9u1kj3e1m0leq1bc1v2jqaf.apps.googleusercontent.com');
+    $client->setClientSecret('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');
+    $client->setRedirectUri('http://localhost:8000/google-drive/callback');
     $code = request('code');
     $access_token = $client->fetchAccessTokenWithAuthCode($code);
     return $access_token;
+});
+
+Route::get('upload', function () {
+    $client = new Client();
+    $access_token = 'ya29.A0ARrdaM9mgF-Fv5mc8KqAViILu5rWdIYd1opltEUf7GOhDw8qBSWNMO6iTkLGtmCtbOH3c3ioHvjjZ3rMlZB1Cmhj__vQK9Qm-5ox-0JfFUYFQBpWyvZfTP06w2B2JSfgSZmUFiErv1jhiSgEQUQcl3ex1nB-';
+
+    $client->setAccessToken($access_token);
+    $service = new Drive($client);
+    $file = new DriveFile();
+
+    DEFINE("TESTFILE", 'test.txt');
+    if (!file_exists(TESTFILE)) {
+        $fh = fopen(TESTFILE, 'w');
+        fseek($fh, 1024 * 1024);
+        fwrite($fh, "!", 1);
+        fclose($fh);
+    }
+
+    $file->setName("Hello World!");
+    $resualt2 = $service->files->create(
+        $file,
+        array(
+            'data' => file_get_contents(TESTFILE),
+            'mimeType' => 'application/octet-stream',
+            'uploadType' => 'multipart'
+        )
+    );
 });
