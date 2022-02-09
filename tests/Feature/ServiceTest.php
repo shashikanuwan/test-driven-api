e63d88fbce4db32b77d7771f7deb0b25a40e8624
shashika nuwan <kumararanaweera1999@gmail.com>
2022-02-09 13:08:06 +0530

    test_service_callback_will_store_token

--- tests/Feature/ServiceTest.php
+++ tests/Feature/ServiceTest.php
@@ -9,13 +9,30 @@ class ServiceTest extends TestCase
 {
     use RefreshDatabase;
 
+    public function setUp(): void
+    {
+        parent::setup();
+        $this->user =  $this->authUser();
+    }
+
     public function test_a_user_can_connect_to_a_service_and_token_is_stored()
     {
-        $this->authUser();
-        $responce = $this->getJson(route('service.connect', 'google-drive'))
+        $responce = $this->getJson(route('web-service.connect', 'google-drive'))
             ->assertOk()
             ->json();
 
         $this->assertNotNull($responce['url']);
+    }
+
+    public function test_service_callback_will_store_token()
+    {
+        $responce = $this->postJson(route('web-service.callback'), [
+            'code' => 'dummy'
+        ])
+            ->assertCreated();
+
+        $this->assertDatabaseHas('web_services', ['user_id' => $this->user->id]);
+
+        // $this->assertNotNull($this->user->services->first()->token);
     }
 }
